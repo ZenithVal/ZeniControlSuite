e9c651b8542eef8e042b545c0ba10f098d19b453
Duinrahaic <gman2306@gmail.com>
2024-03-29 14:39:55 -0500

    Updated to allow syncing for points and built example tree component

---
+++ Components/Pages/Page_Games_Points.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ZeniControlSuite.Components.Pages;
+
+public partial class Page_Games_Points : IDisposable
+{
+    
+    public static bool pageEnabled = true;
+
+    [Inject] 
+    private PointsService Points { get; set; } = default!;
+    
+    
+    protected override void OnInitialized()
+    {
+ 
+    }
+    
+    
+    
+    private void OnPointsUpdate()
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void btnAddWhole()
+    {
+        Points.UpdatePoints(1.0);
+    }
+
+    private void btnAddThird()
+    {
+        Points.UpdatePoints(1.0 / 3);
+    }
+
+    private void btnAddFourth()
+    {
+        Points.UpdatePoints(1.0 / 4);
+    }
+
+    private void btnSubWhole()
+    {
+        Points.UpdatePoints(-1);
+    }
+
+    private void btnSubThird()
+    {
+        Points.UpdatePoints(-1.0 / 3);
+    }
+
+    private void btnSubFourth()
+    {
+        Points.UpdatePoints(-1.0 / 4);
+    }
+    
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
