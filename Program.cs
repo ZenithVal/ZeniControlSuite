8778b8d04d6a9e88f5e5bdda9410a4302b6390e8
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2024-07-23 01:12:55 -0400

    Minor cleanup

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ using ZeniControlSuite.Authentication;
 using ZeniControlSuite.Components;
 using ZeniControlSuite.Services;
 
+
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
