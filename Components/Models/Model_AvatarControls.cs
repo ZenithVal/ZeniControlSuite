984660f85a1b57085c5ce27fdda15c4580ed93af
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2024-07-23 23:59:55 -0400

    Avatar Controls Setup

---
+++ Components/Models/Model_AvatarControls.cs
@@ -0,0 +1,77 @@
+﻿namespace ZeniControlSuite.Components.Avatars;
+
+public class Avatar
+{
+	public string ID { get; set; }
+    public string Name { get; set; }
+    public List<Control> Controls { get; set; }
+}
+
+public enum ControlType
+{
+	Button,
+	Toggle,
+	Radial,
+	HSV,
+}
+public abstract class Control
+{
+	public string Name { get; set; }
+	public abstract ControlType Type { get; }
+	public List<String> RequiredRoles { get; set; } = new List<String>();
+}
+public class ContTypeButton : Control
+{
+	public override ControlType Type => ControlType.Button;
+	public required Parameter Parameter { get; set; }
+}
+public class ContTypeToggle : Control
+{
+	public override ControlType Type => ControlType.Toggle;
+	public required Parameter Parameter { get; set; }
+	public float ValueOff { get; set; }
+	public float ValueOn { get; set; }
+}
+public class ContTypeRadial : Control
+{
+	public override ControlType Type => ControlType.Radial;
+	public required Parameter Parameter { get; set; }
+	public float ValueMin { get; set; }
+	public float ValueMax { get; set; }
+}
+public class ContTypeHSV : Control
+{
+	public override ControlType Type => ControlType.HSV;
+	public required Parameter ParameterHue { get; set; }
+	public required Parameter ParameterSaturation { get; set; }
+	public required Parameter ParamterBrightness { get; set; }
+	public bool InvertedBrightness { get; set; }
+}
+
+
+public enum ParameterType
+{
+	Bool,
+	Int,
+	Float
+}
+public abstract class Parameter
+{
+	public string Path { get; set; }
+	public abstract ParameterType Type { get; }
+}
+public class ParamTypeBool : Parameter
+{
+	public override ParameterType Type => ParameterType.Bool;
+	public bool Value { get; set; }
+}
+public class ParamTypeInt : Parameter
+{
+	public override ParameterType Type => ParameterType.Int;
+	public int Value { get; set; }
+}
+public class ParamTypeFloat : Parameter
+{
+	public override ParameterType Type => ParameterType.Float;
+	public float Value { get; set; }
+}
---
+++ Services/Service_AvatarControls.cs
@@ -0,0 +1,171 @@
+﻿using MudBlazor;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Components;
+using ZeniControlSuite.Components.Avatars;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZeniControlSuite.Services;
+public class Service_AvatarControls : IHostedService
+{
+    [Inject] private Service_Points PointsService { get; set; } = default!;
+    [Inject] private Service_Logs LogService { get; set; } = default!;
+    private void Log(string message, Severity severity)
+    {
+        LogService.AddLog("Service_AvatarControls", "System", message, severity);
+    }
+
+    //===========================================//
+    #region HostedService Stuff 
+    public delegate void RequestAvatarsUpdate();
+    public event RequestAvatarsUpdate? OnAvatarsUpdate;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+    public void InvokeAvatarControlsUpdate()
+    {
+        if (OnAvatarsUpdate != null)
+        {
+            OnAvatarsUpdate.Invoke();
+        }
+    }
+    #endregion
+
+
+    //===========================================//
+    public List<Control> globalControls = new List<Control>();
+    public List<Avatar> avatars = new List<Avatar>();
+
+    //===========================================//
+    #region Load Avatars From json
+    public void ReadAvatarControlsJson(string jsonString)
+    {
+        // Deserialize the JSON string
+        var jsonDocument = JsonDocument.Parse(jsonString);
+
+        // Deserialize GlobalControls
+        var globalControlsElement = jsonDocument.RootElement.GetProperty("GlobalControls");
+        foreach (var controlElement in globalControlsElement.EnumerateArray())
+        {
+            var control = DeserializeControl(controlElement);
+            globalControls.Add(control);
+        }
+
+        // Deserialize Avatars
+        var avatarsElement = jsonDocument.RootElement.GetProperty("Avatars");
+        foreach (var avatarElement in avatarsElement.EnumerateArray())
+        {
+            var avatar = new Avatar {
+                ID = avatarElement.GetProperty("ID").GetString(),
+                Name = avatarElement.GetProperty("Name").GetString(),
+                Controls = new List<Control>()
+            };
+
+            // Deserialize Avatar Controls
+            var controlsElement = avatarElement.GetProperty("Controls");
+            foreach (var controlElement in controlsElement.EnumerateArray())
+            {
+                var control = DeserializeControl(controlElement);
+                avatar.Controls.Add(control);
+            }
+
+            // Deserialize Inherited Global Controls
+            var inheritedControlsElement = avatarElement.GetProperty("InheritedGlobalControls");
+            foreach (var controlName in inheritedControlsElement.EnumerateArray())
+            {
+                var globalControl = globalControls.FirstOrDefault(c => c.GetType().Name == controlName.GetString());
+                if (globalControl != null)
+                {
+                    avatar.Controls.Add(globalControl);
+                }
+            }
+
+            avatars.Add(avatar);
+        }
+    }
+
+    private Control DeserializeControl(JsonElement controlElement)
+    {
+        var type = controlElement.GetProperty("Type").GetString();
+        Control control;
+
+        switch (type)
+        {
+            case "Button":
+                control = new ContTypeButton {
+                    Parameter = DeserializeParameter(controlElement.GetProperty("Parameter"))
+                };
+                break;
+            case "Toggle":
+                control = new ContTypeToggle {
+                    Parameter = DeserializeParameter(controlElement.GetProperty("Parameter")),
+                    ValueOff = controlElement.GetProperty("ValueOff").GetSingle(),
+                    ValueOn = controlElement.GetProperty("ValueOn").GetSingle()
+                };
+                break;
+            case "Slider":
+                control = new ContTypeRadial {
+                    Parameter = DeserializeParameter(controlElement.GetProperty("Parameter")),
+                    ValueMin = controlElement.GetProperty("ValueMin").GetSingle(),
+                    ValueMax = controlElement.GetProperty("ValueMax").GetSingle()
+                };
+                break;
+            case "HSV":
+                control = new ContTypeHSV {
+                    ParameterHue = DeserializeParameter(controlElement.GetProperty("ParameterHue")),
+                    ParameterSaturation = DeserializeParameter(controlElement.GetProperty("ParameterSaturation")),
+                    ParamterBrightness = DeserializeParameter(controlElement.GetProperty("ParamterBrightness")),
+                    InvertedBrightness = controlElement.GetProperty("InvertedBrightness").GetBoolean()
+                };
+                break;
+            default:
+                throw new InvalidOperationException("Unknown control type.");
+        }
+
+        return control;
+    }
+
+    private Parameter DeserializeParameter(JsonElement parameterElement)
+    {
+        var type = parameterElement.GetProperty("Type").GetString();
+        Parameter parameter;
+
+        switch (type)
+        {
+            case "Bool":
+                parameter = new ParamTypeBool {
+                    Path = parameterElement.GetProperty("Path").GetString(),
+                    Value = parameterElement.GetProperty("Value").GetBoolean()
+                };
+                break;
+            case "Int":
+                parameter = new ParamTypeInt {
+                    Path = parameterElement.GetProperty("Path").GetString(),
+                    Value = parameterElement.GetProperty("Value").GetInt32()
+                };
+                break;
+            case "Float":
+                parameter = new ParamTypeFloat {
+                    Path = parameterElement.GetProperty("Path").GetString(),
+                    Value = parameterElement.GetProperty("Value").GetSingle()
+                };
+                break;
+            default:
+                Log("Unknown parameter type", Severity.Error);
+                throw new InvalidOperationException("Unknown parameter type");
+        }
+
+        return parameter;
+    }
+    #endregion
+
+    //===========================================//
+}
+
